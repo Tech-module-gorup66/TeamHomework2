28a2b8ff8dc1e5f1f5be3a0e3d477412cabb118c
Andrew <a.kr.andreev@gmail.com>
2018-03-18 13:55:14 +0200

    add new problem

---
+++ intro-and- basic-syntax/Excercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicSytaxExercises2301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+            var num3 = int.Parse(Console.ReadLine());
+            var num4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1:D4} {num2:D4} {num3:D4} {num4:D4}");
+
+        }
+    }
+}
